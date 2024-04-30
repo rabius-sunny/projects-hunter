b1bcc0c35ed49965ef403ae2c2ea9b4b0c21a715
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-04-30 18:38:16 +0600

    add: seeding data

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+type TProject = {
+  id: string
+  title: string
+  desctiption: string
+  deadline: Date
+  team: string[] // IDs of members
+  tasks: string[] // IDs of tasks
+  //   recents?: any
+}
+
+type TTask = {
+  id: string
+  title: string
+  description: string
+  deadline: Date
+  assignee: string[] // IDs of members
+  status: 'PLANNING' | 'COOKING' | 'EAT'
+}
+
+type TMember = {
+  id: string
+  name: string
+  designation: string
+}
